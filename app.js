ba3eb899369bcc3ae10f8969f35387387fad5673
Decio Soares <decio.msoares@gmail.com>
2020-11-11 11:59:28 +0000

    create logger util

--- app.js
+++ app.js
@@ -3,32 +3,17 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const cors = require('cors')
-const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs.js')
+const logger = require('./utils/logger.js')
 
 const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-    .then(() => console.log('connected to mongodb'))
-    .catch(error => console.error(error))
+    .then(() => logger.info('connected to mongodb'))
+    .catch(error => logger.error('error connecting to mongodb:', error.message))
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-      Blog
-        .find({})
-        .then(blogs => {
-                  response.json(blogs)
-                })
-})
-
-app.post('/api/blogs', (request, response) => {
-      const blog = new Blog(request.body)
-
-      blog
-        .save()
-        .then(result => {
-                  response.status(201).json(result)
-                })
-})
+app.use('/', blogsRouter)
 
 module.exports = app
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 require('dotenv').config()
 const http = require('http')
 const app = require('./app')
+const logger = require('./utils/logger.js')
 
 const server = http.createServer(app)
 
 const PORT = process.env.PORT
 server.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`)
+      logger.info(`Server running on port ${PORT}`)
 })
