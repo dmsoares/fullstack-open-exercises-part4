4e87c09e6acd74ce03ee13c76853bccef129a78f
Decio Soares <decio.msoares@gmail.com>
2020-11-12 23:11:38 +0000

    add 4.6 solution

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,30 +1,45 @@
 const dummy = blogs => {
-    return 1
+  return 1
 }
 
 const totalLikes = blogs => {
-    return blogs.reduce((sum, blog) => {
-        return sum += blog.likes
-    }, 0)
+  return blogs.reduce((sum, blog) => {
+    return sum += blog.likes
+  }, 0)
 }
 
 const favoriteBlog = blogs => {
-    if (!blogs.length) return -1
+  if (!blogs.length) return -1
 
-    const likes = blogs.map(blog => blog.likes)
-    const favorite = blogs.find(blog => {
-        return blog.likes === (Math.max(...likes))
-    })
+  const likes = blogs.map(blog => blog.likes)
+  const favorite = blogs.find(blog => {
+    return blog.likes === (Math.max(...likes))
+  })
 
-    return {
-        title: favorite.title,
-        author: favorite.author,
-        likes: favorite.likes
-    }
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes
+  }
+}
+
+const mostBlogs = blogs => {
+  if (!blogs.length) return -1
+  const blogsPerAuthor = []
+
+  blogs.forEach(blog => {
+    const index = blogsPerAuthor.findIndex(element => element.author === blog.author)
+    index > -1 ? blogsPerAuthor[index].blogs++ : blogsPerAuthor.push({ author: blog.author, blogs: 1 })  
+  })
+
+  return blogsPerAuthor.find(author => {
+    return author.blogs === Math.max(...Object.values(blogsPerAuthor.map(author => author.blogs)))
+  })
 }
 
 module.exports = {
-    dummy,
-    totalLikes,
-    favoriteBlog
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs
 }
